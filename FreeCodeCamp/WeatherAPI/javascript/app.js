ee73d2b133c90585bffeb24d4c8e568c070f1e06
Ivan Santos <ivandega22@gmail.com>
2018-05-16 02:09:43 +0200

    Update app.js

--- FreeCodeCamp/WeatherAPI/javascript/app.js
+++ FreeCodeCamp/WeatherAPI/javascript/app.js
@@ -3,38 +3,31 @@ var browserLocation;
 var foundWeather;
 
 
-$(document).ready(function(){
-	browserLocation = getOwnLocation();
+$(document).ready(() => { browserLocation = getOwnLocation();
 	//Añadimos la imagen de carga en el contenedor
-    $('<img src="images/loading.gif"/>').insertAfter('.content');
-
+        $('<img src="images/loading.gif"/>').insertAfter('.content');
 });
 
 function searchWeather(){
 	var city = $(".city").val();
-	$.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`, function(json){
+	$.getJSON(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`, (json) => {
 		foundWeather = json;
 		$(".name").text(`${foundWeather.name}, ${foundWeather.sys.country}`);
 		$(".temperature").text(`${browserLocation.main.temp} °C`);
 		$(".description").text(foundWeather.weather[0].description);
-
 	});
-
 	return false;
 }
 
-
 function getOwnLocation() {
-	navigator.geolocation.getCurrentPosition((pos) => getJSON(pos));
+	navigator.geolocation.getCurrentPosition((position) => getJSON(position));
 }
 
-
-function getJSON(pos){
+function getJSON(position){
       //Ajax calling Json
-		$.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&appid=${API_KEY}&units=metric`, 
-		  function(json){
-     		browserLocation = json;
-
+		$.getJSON(`http://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${API_KEY}&units=metric`, 
+		  (json) => {
+     			browserLocation = json;
 			$('img').fadeOut(1000, () => {
 				$(".name").text(`${browserLocation.name}, ${browserLocation.sys.country}`);
 				$(".temperature").text(`${browserLocation.main.temp} °C`);
@@ -42,8 +35,3 @@ function getJSON(pos){
 			});
 		  });
 };
-
-
-function generateText(){
-}
-
